85f39b3b11f6037b172422d37d280a6454786452
Bylvin <56466617+Bylvin@users.noreply.github.com>
2021-01-18 13:00:42 +0700

    Menambahkan splash activity yang sudah jadi

--- app/src/main/java/com/example/medistoasebiocompanionapp/SplashActivity.java
+++ app/src/main/java/com/example/medistoasebiocompanionapp/SplashActivity.java
@@ -2,13 +2,52 @@ package com.example.medistoasebiocompanionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //Variabel
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView slogan, tagline;
+
+    private static int SPLASH_SCREEN_DURATION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Untuk menghilangkan status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        //Mengisi variabel
+        logo = findViewById(R.id.logo_medis_toasebio);
+        slogan = findViewById(R.id.slogan_app_name);
+        tagline = findViewById(R.id.tagline_app);
+
+        //Animasi
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+        tagline.setAnimation(bottomAnim);
+
+        //Untuk pindah ke menu login
+            //untuk nahan berapa lama durasi splash screen
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run()
+                {
+                    Intent intent = new Intent(SplashActivity.this, LoginMemberActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_SCREEN_DURATION);
     }
 }
